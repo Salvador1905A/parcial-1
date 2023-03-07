67a3546ba145bd4ac4144f4bc8704c2ac0af590c
Salvador1905A <127062704+Salvador1905A@users.noreply.github.com>
2023-03-07 15:43:54 -0600

    Add files via upload

---
+++ nodejs-mysql-railway-sca/nodejs-mysql-railway-master/src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 19990
+
+export const DB_HOST = process.env.DB_HOST || 'mysql-114461-0.cloudclusters.net'
+export const DB_USER = process.env.DB_USER || 'Salvador'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'salvador19'
+export const DB_NAME = process.env.DB_NAME || 'user'
+export const DB_PORT = process.env.DB_PORT || 3306
