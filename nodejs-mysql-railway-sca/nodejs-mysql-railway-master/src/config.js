9b677223da0f8f2152a2c5f5017ad2c005e39b42
SalvadorCA <127062704+Salvador1905A@users.noreply.github.com>
2023-03-07 16:35:33 -0600

    Update config.js

--- nodejs-mysql-railway-sca/nodejs-mysql-railway-master/src/config.js
+++ nodejs-mysql-railway-sca/nodejs-mysql-railway-master/src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 19990
 
-export const DB_HOST = process.env.DB_HOST || 'mysql-114461-0.cloudclusters.net'
-export const DB_USER = process.env.DB_USER || 'Salvador'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'salvador19'
-export const DB_NAME = process.env.DB_NAME || 'user'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_HOST = process.env.DB_HOST || 'mysql://root:mfeA3LdrEV9cvC11asfF@containers-us-west-115.railway.app:7075/railway'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'mfeA3LdrEV9cvC11asfF'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 7075
